7eb63df7f32f01f6a70ef08e1caada360bea4b34
boo1ean <boo1ean0807@gmail.com>
2016-01-14 23:39:28 +0200

    Change debug module name

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var assert = require('assert');
-var debug = require('debug')('github-hooks');
+var debug = require('debug')('github-events');
 
 function startServer (opts) {
 	assert(opts.port, 'port is required to start server');
